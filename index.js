b8151cfef56d5484e003f4dc968399e02621e154
Andrei Pauliukovich <apavlukovich@gmail.com>
2021-04-02 16:15:12 +0300

    feat: search panel

---
+++ index.js
@@ -0,0 +1,24 @@
+const refreshButton = document.querySelector(".refresh-btn");
+const bodyBackground = document.querySelector("body");
+
+function refreshBackground() {
+    bodyBackground.style.add('body::content');
+}
+
+refreshBackground.addEventListener("click", refreshBackground);
+
+
+ymaps.ready(init);
+    function init(){
+        // Создание карты.
+        var myMap = new ymaps.Map("map", {
+            // Координаты центра карты.
+            // Порядок по умолчанию: «широта, долгота».
+            // Чтобы не определять координаты центра карты вручную,
+            // воспользуйтесь инструментом Определение координат.
+            center: [55.76, 37.64],
+            // Уровень масштабирования. Допустимые значения:
+            // от 0 (весь мир) до 19.
+            zoom: 7
+        });
+    }
