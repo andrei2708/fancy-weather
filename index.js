12acff5950fc0e5e295d1136850d34467ea6506c
Andrei Pauliukovich <apavlukovich@gmail.com>
2021-04-15 12:31:40 +0300

    fix: fixed error name day

--- index.js
+++
@@ -1,8 +0,0 @@
-const refreshButton = document.querySelector(".refresh-btn");
-const bodyBackground = document.querySelector("body");
-
-function refreshBackground() {
-    bodyBackground.style.add('body::content');
-}
-
-refreshBackground.addEventListener("click", refreshBackground);
